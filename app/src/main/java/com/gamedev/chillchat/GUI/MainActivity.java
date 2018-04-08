307b0826ba5f07fac54c052c0ff79da073668bf1
Алексей <korneev.ry@yandex.ru>
2018-04-09 03:42:04 +0400

    Стабильная работа

--- app/src/main/java/com/gamedev/chillchat/GUI/MainActivity.java
+++ app/src/main/java/com/gamedev/chillchat/GUI/MainActivity.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -53,21 +51,6 @@ public class MainActivity extends AppCompatActivity {
             client.setSocket(null);
         }
         client.setConnection(IP, String.valueOf(PORT));
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.menu_settings) {
-            //Intent intent = new Intent(this, MenuActivity.class);
-            // startActivity(intent);
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     public void goToChat() {
